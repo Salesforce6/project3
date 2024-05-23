2072f452e286973ec1bbe0f1320a8be6d04717cc
Andrew Tran <tranandrew.tech@gmail.com>
2024-05-23 10:48:00 -0500

    add manifest folder

--- force-app/main/default/lwc/propertyHome/propertyHome.js
+++ force-app/main/default/lwc/propertyHome/propertyHome.js
@@ -26,18 +26,18 @@ export default class PropertyHome extends LightningElement {
         }
     }
 
-    navToPropertyPage(e) {
+    // navToPropertyPage(e) {
 
-        // the name Residence_Viewer__c needs to correspond with
-        // the api name of the page made in experience builder
-        this[NavigationMixin.Navigate]({
-            type: 'comm__namedPage',
-            attributes: {
-                name: 'Residence_Viewer__c',
-            },
-            state: {
-                c__recordId: e.target.dataset.id
-            }
-        });
-    }
+    //     // the name Residence_Viewer__c needs to correspond with
+    //     // the api name of the page made in experience builder
+    //     this[NavigationMixin.Navigate]({
+    //         type: 'comm__namedPage',
+    //         attributes: {
+    //             name: 'Residence_Viewer__c',
+    //         },
+    //         state: {
+    //             c__recordId: e.target.dataset.id
+    //         }
+    //     });
+    // }
 }
