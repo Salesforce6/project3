968b02660dd1ed696ba7dd5178a007c5e6769e71
miguellangel <arriaga.miguel123@gmail.com>
2024-05-24 03:22:58 -0500

    tour nav button complete

--- force-app/main/default/lwc/hero_component/hero_component.js
+++ force-app/main/default/lwc/hero_component/hero_component.js
@@ -4,22 +4,23 @@ import { NavigationMixin } from 'lightning/navigation';
 export default class Hero_Component extends NavigationMixin(LightningElement) {
 
     navToSeeAllPropertiesPage(e) {
-        // the name Residence_Viewer__c needs to correspond with
+        // the name Property__c needs to correspond with
         // the api name of the page made in experience builder
         this[NavigationMixin.Navigate]({
-            type: 'comm__namedPage',
+            type: 'standard__objectPage',
             attributes: {
-                name: 'Property_Detail__c',
+                objectApiName: 'Property__c',
+                actionName: 'list'
             }
         });
     }
 
-    // navToBookATour(e) {
-    //     this[NavigationMixin.Navigate]({
-    //         type: 'comm__namedPage',
-    //         attributes: {
-    //             name: 'Tour__c'
-    //         }
-    //     })
-    // }
+    navToBookATour(e) {
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'tour__c'
+            }
+        })
+    }
 }
