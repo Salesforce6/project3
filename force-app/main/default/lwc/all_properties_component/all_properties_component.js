198240c223342c9f9cba30f4c59f1febe4fc86e1
miguellangel <arriaga.miguel123@gmail.com>
2024-05-24 03:05:05 -0500

    hero to properties nav complete

---
+++ force-app/main/default/lwc/all_properties_component/all_properties_component.js
@@ -0,0 +1,3 @@
+import { LightningElement } from 'lwc';
+
+export default class All_properties_component extends LightningElement {}
--- force-app/main/default/lwc/hero_component/hero_component.js
+++ force-app/main/default/lwc/hero_component/hero_component.js
@@ -1,27 +1,25 @@
 import { LightningElement, track, wire } from 'lwc';
-import getAllVacantProperties from '@salesforce/apex/PropertyHelper.getAllPropertiesWithVacancies'
 import { NavigationMixin } from 'lightning/navigation';
-import isGuest from '@salesforce/user/isGuest';
-// import heroImage from '@salesforce/resourceUrl/heroImage;
 
-export default class Home extends NavigationMixin(LightningElement) {
+export default class Hero_Component extends NavigationMixin(LightningElement) {
 
-    @wire(getAllVacantProperties)
-    properties;
+    navToSeeAllPropertiesPage(e) {
+        // the name Residence_Viewer__c needs to correspond with
+        // the api name of the page made in experience builder
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'Property_Detail__c',
+            }
+        });
+    }
 
-
-    // navToResidencePage(e) {
-
-    //     // the name Residence_Viewer__c needs to correspond with
-    //     // the api name of the page made in experience builder
+    // navToBookATour(e) {
     //     this[NavigationMixin.Navigate]({
     //         type: 'comm__namedPage',
     //         attributes: {
-    //             name: 'Residence_Viewer__c',
-    //         },
-    //         state: {
-    //             c__recordId: e.target.dataset.id
+    //             name: 'Tour__c'
     //         }
-    //     });
+    //     })
     // }
 }
