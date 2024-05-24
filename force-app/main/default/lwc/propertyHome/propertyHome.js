5f40965cf0a053825976b3b16b09b8446d09f6d7
Andrew Tran <tranandrew.tech@gmail.com>
2024-05-24 09:28:26 -0500

    pre commit merge

--- force-app/main/default/lwc/propertyHome/propertyHome.js
+++ force-app/main/default/lwc/propertyHome/propertyHome.js
@@ -1,12 +1,12 @@
 import { LightningElement, wire } from 'lwc';
 import getPopularPropertiesRecords from '@salesforce/apex/PropertiesHomeController.getPopularPropertiesRecords';
-import propertyImg from '@salesforce/resourceUrl/nameOfImg';
+// import propertyImg from '@salesforce/resourceUrl/nameOfImg';
 import { NavigationMixin } from 'lightning/navigation';
 
-export default class PropertyHome extends LightningElement {
+export default class PropertyHome extends NavigationMixin(LightningElement) {
 
-    propertiesToDisplay = [];
-    sfiage  = propertyImg;
+    propertiesToDisplay;
+    // sfiage  = propertyImg;
 
     @wire(getPopularPropertiesRecords)
     GetPropertiesHandler(response) {
@@ -26,18 +26,18 @@ export default class PropertyHome extends LightningElement {
         }
     }
 
-    // navToPropertyPage(e) {
+    navToPropertyPage(e) {
 
-    //     // the name Residence_Viewer__c needs to correspond with
-    //     // the api name of the page made in experience builder
-    //     this[NavigationMixin.Navigate]({
-    //         type: 'comm__namedPage',
-    //         attributes: {
-    //             name: 'Residence_Viewer__c',
-    //         },
-    //         state: {
-    //             c__recordId: e.target.dataset.id
-    //         }
-    //     });
-    // }
+        // the name Residence_Viewer__c needs to correspond with
+        // the api name of the page made in experience builder
+        this[NavigationMixin.Navigate]({
+            type: 'comm__namedPage',
+            attributes: {
+                name: 'Property_Detail__c',
+            },
+            state: {
+                c__recordId: e.target.dataset.id
+            }
+        });
+    }
 }
